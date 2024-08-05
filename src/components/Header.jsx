37007750c06295b5ed0349dddc1ea678aac3943f
code-looney <danielvbofficieel@gmail.com>
2024-08-05 05:57:45 +0200

    nav mobile desk done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,32 +12,25 @@ const Header = () => {
 
   return (
     <header className='w-full h-[88px] flex justify-between items-center tracking-widest relative outline'>
-    <div className='relative gap-[64px] items-center z-40 lg:min-w-[160px] pl-[1.5em] flex'>
-        <Link className='w-full'><span className='uppercase text-2xl w-full font-bold text-[#00df9a]'>Code Looney</span></Link>
+    <div className='relative gap-[64px] w-full items-center z-40 lg:min-w-[160px] pl-[1.5em] flex'>
+        <Link className='w-full'><span className='uppercase text-xl w-full font-bold text-[#00df9a]'>Code Looney</span></Link>
         <hr className='w-full translate-x-32 hidden absolute' />
     </div>
-    <div className=' h-[40px] w-[187.5px] flex justify-end items-center right-0 top-0 pr-[1.5em] '>
+    <div className='h-[40px] w-[187.5px] flex justify-end items-center right-0 top-0 pr-[1.5em] md:hidden'>
         <Link onClick={() => context.handleToggleMobileMenu()}><img src="public/images/icon-hamburger.svg" alt="hamburger icon" /></Link>
     </div>
-    <ul className='h-full flex-row justify-end z-10 items-center pr-[3em] hidden gap-[3em] text-[1rem] relative text-white uppercase'>
-    <li className=' w-full hidden'>
-    <hr className='w-full text-[#979797] hidden' />
-    </li>
-      {context.router && context.router.map(route => {
-        return (
-          <li key={route.id} className={`h-full items-center flex-col justify-center relative z-20 border-b-4 flex gap-3 ${context.hoverUnderlineColor === route.id ? null : "hover:border-opacity-50 "} ease-in-out duration-700 hover:border-b-white 
-          ${context.hoverUnderlineColor === route.id ? 'border-b-white' : 'border-b-transparent'}`}>
-          <NavLink to={`/`}
-              onClick={() => context.handleNavLinkHover(route.id)}
-              className={`flex gap-3`}>
-              {/* <span className={`font-bold`}></span>  */}
-              <span className={`${route.order === "00" ? "hidden" : ""} ${route.order === "00" ? "xl:block" : ""}`}>{route.order}</span>
-            {route.page}
-              </NavLink>
+          {/* Desktop Navigation */}
+          <ul className='hidden md:flex'>
+        {context.router && context.router.map(item => (
+          <li
+            key={item.id}
+            className='p-4 hover:bg-[#00df9a] text-white rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
+          >
+            {item.page}
           </li>
-        )
-      })}
-    </ul>
+        ))}
+      </ul>
+
 </header>
   )
 }
--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -17,9 +17,10 @@ const MobileMenu = () => {
           leaveFrom="transform translate-x-0 opacity-100"
           leaveTo="transform translate-x-full opacity-0"
           >
-            <div className={`flex-col fixed right-0 top-0 w-[50%] h-full border-l border-l-gray-900 bg-[#000300] ease-in-out duration-500
+            <div className={`flex-col md:hidden fixed right-0 top-0 w-[50%] h-full border-l border-l-gray-900 bg-[#000300] ease-in-out duration-500
               ${context.toggleMobileMenu === false ? `transition transform duration-700 ease-in-out translate-x-0 opacity-100` : "" }
                text-white backdrop-blur-xl z-40 bg-white/5`}>
+                {/* zoek een oplossing om de menu te sluiten wanneer md bereikt */}
                 <ul className='transition duration-300 ease-in-out '>
                     <li className='h-[88px] flex justify-end items-center pr-[1.5em] '>
                         <button aria-expanded={context.toggleMobileMenu}  onClick={context.handleToggleMobileMenu}>
