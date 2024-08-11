435493c36207a19052b8bd76a8e7211bd19cd221
code-looney <danielvbofficieel@gmail.com>
2024-08-11 16:54:25 +0200

    mod mobile menu transition

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -11,7 +11,7 @@ const MobileMenu = () => {
     };
 
     return (
-        <Transition
+            <Transition
             show={context.toggleMobileMenu}
             enter="transition-transform duration-700 ease-in-out"
             enterFrom="transform translate-x-full opacity-0"
@@ -19,10 +19,11 @@ const MobileMenu = () => {
             leave="transition-transform duration-700 ease-in-out"
             leaveFrom="transform translate-x-0 opacity-100"
             leaveTo="transform translate-x-full opacity-0"
-        >
-            <div className={`fixed right-0 py-7 top-0 w-[50%] h-full border-l border-l-gray-900 bg-[#0000001f] ease-in-out duration-500
-              text-white backdrop-blur-xl z-40 bg-white/5`}>
-                <ul className='transition duration-300 ease-in-out'>
+            >
+            <div className={`flex-col md:hidden fixed right-0 py-7 top-0 w-[50%] h-full border-l border-l-gray-900 bg-[#0000001f] ease-in-out duration-500
+              ${context.toggleMobileMenu === false ? `transition transform duration-700 ease-in-out translate-x-0 opacity-100` : "" }
+               text-white backdrop-blur-xl z-40`}>
+                <ul>
                     <li className='flex justify-end items-center pr-[1.5em]'>
                         <button aria-expanded={context.toggleMobileMenu} onClick={context.handleToggleMobileMenu}>
                             <img className='w-[24px]' src={`/images/icon-close.svg`} alt="Close menu icon" />
@@ -56,10 +57,10 @@ const MobileMenu = () => {
                                             leaveFrom="opacity-100 max-h-[500px]"
                                             leaveTo="opacity-0 max-h-0"
                                         >
-                                            <div className="ml-5 ">
+                                            <div className="border-b  border-gray-600">
                                                 <NavLink
                                                     to="/one-to-one-coaching-call"
-                                                    className="block border border-gray-600 rounded-xl p-4 text-[14px] font-barlowCondensed hover:bg-[#00df9a] hover:text-black duration-300 w-fullr"
+                                                    className="block border border-gray-600 rounded-xl p-4 ml-5 text-[.8rem] font-barlowCondensed hover:bg-[#00df9a] hover:text-black duration-300 w-fullr"
                                                 >
                                                     1 To 1 Call
                                                 </NavLink>
